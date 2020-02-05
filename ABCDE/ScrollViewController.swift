e39f9416b20326051148b047f1df45bebdf01199
KosekiTakashi <takashi111398@gmail.com>
2020-02-06 02:09:35 +0900

    Add Code

---
+++ ABCDE/ScrollViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ScrollViewController.swift
+//  ABCDE
+//
+//  Created by 小関隆司 on 2020/02/03.
+//  Copyright © 2020 kosekitakashi. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var posX: CGFloat!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+extension ScrollViewController: UIScrollViewDelegate{
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        posX = scrollView.contentOffset.x
+        
+        
+    }
+
+    private func scrollViewDidScroll(scrollView: UIScrollView) {
+        scrollView.contentOffset.x = posX
+    }
+    
+    
+}
